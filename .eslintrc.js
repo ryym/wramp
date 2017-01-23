e4a5d52083787825554fdafe8f47652f88f4915c
ryym <ryym.64@gmail.com>
2017-01-23 21:43:16 +0900

    chore: Set up Babel and ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "root": true,
+  "extends": "ryym/base",
+
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    }
+  }
+}
+

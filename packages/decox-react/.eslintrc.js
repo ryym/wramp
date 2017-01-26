a4f9a400a0d17229de0900eba1bb00b93f075bc8
ryym <ryym.64@gmail.com>
2017-01-26 21:07:38 +0900

    Set up decox-react package

---
+++ packages/decox-react/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "root": true,
+  "extends": "ryym/base-react",
+
+  "plugins": [
+    "react"
+  ],
+
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    }
+  }
+}
+

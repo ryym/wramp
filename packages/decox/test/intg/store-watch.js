ef2b431acf5bb37c0a404510a8e6099bdd3c815d
ryym <ryym.64@gmail.com>
2017-01-28 15:47:24 +0900

    test(decox): Add simple integration test
    The code transpiled by Babel with `es2015` preset
    throws an error on Node.js while it works fine on browsers.
    
    error: Class constructor State cannot be invoked without 'new'
    
    So configure Babel per environments temporarily.

---
+++ packages/decox/test/intg/store-watch.js
@@ -0,0 +1,77 @@
+import test from 'ava';
+import sinon from 'sinon';
+import { defineStore, watch } from '../../src';
+
+class State {
+  constructor(init = 0) {
+    this.value = init;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  $setValue(value) {
+    this.value = value;
+  }
+
+  $$addAsync(n) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.$setValue(this.value + n);
+        resolve();
+      });
+    });
+  }
+}
+
+const Store = defineStore(State);
+
+const watchBySpy = store => {
+  const handlers = {
+    update: sinon.spy(),
+    action: sinon.spy(),
+  };
+  watch(store).onUpdate(({ method, payload }) => {
+    handlers.update({ method, payload });
+  });
+  watch(store).onAction(({ method, payload }) => {
+    handlers.action({ method, payload });
+  });
+  return handlers;
+};
+
+test('intg: calls normal method', t => {
+  const store = new Store(1);
+  t.is(store.getValue(), 1);
+});
+
+test('intg: calls update method', t => {
+  const store = new Store(1);
+  const handlers = watchBySpy(store);
+
+  store.$setValue(2);
+  t.deepEqual(handlers.update.args[0], [{
+    method: '$setValue',
+    payload: [2],
+  }]);
+  t.is(store.getValue(), 2);
+});
+
+test('intg: calls action method', async t => {
+  const store = new Store(1);
+  const handlers = watchBySpy(store);
+
+  t.plan(3);
+  await store.$$addAsync(10).then(() => {
+    t.deepEqual(handlers.action.args[0], [{
+      method: '$$addAsync',
+      payload: [10],
+    }]);
+    t.deepEqual(handlers.update.args[0], [{
+      method: '$setValue',
+      payload: [11],
+    }]);
+    t.is(store.getValue(), 11);
+  });
+});

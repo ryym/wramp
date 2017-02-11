4f336050eab6f00423eff733ae28a620db895c87
ryym <ryym.64@gmail.com>
2017-02-11 15:27:34 +0900

    example(async): Fix logging

--- packages/wramp-react/examples/async/src/index.js
+++ packages/wramp-react/examples/async/src/index.js
@@ -5,11 +5,11 @@ import store, { watcher } from './store';
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
-  watcher.onEffect(({ from, methodName }) => {
-    console.log(`Effect: ${methodName} (${from})`);
+  watcher.onEffect(({ originalClassName, methodName }) => {
+    console.log(`Effect: ${methodName} (${originalClassName})`);
   });
-  watcher.onUpdate(({ from, methodName }) => {
-    console.log(`Update: ${methodName} (${from})`, store.takeSnapshot());
+  watcher.onUpdate(({ originalClassName, methodName }) => {
+    console.log(`Update: ${methodName} (${originalClassName})`, store.takeSnapshot());
   });
 }
 

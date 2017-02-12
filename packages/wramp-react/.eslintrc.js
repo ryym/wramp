6dd3b8e92234ecb51c44ec5da78453ee438f4e1e
ryym <ryym.64@gmail.com>
2017-02-12 14:46:13 +0900

    chore: Install ESLint as root module

--- packages/wramp-react/.eslintrc.js
+++ packages/wramp-react/.eslintrc.js
@@ -1,16 +1,8 @@
 module.exports = {
-  "root": true,
   "extends": "ryym/base-react",
 
   "plugins": [
     "react"
   ],
-
-  "parserOptions": {
-    "ecmaVersion": 2017,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true
-    }
-  }
 }
 

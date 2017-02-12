9a2446e18277682a46ca0cd5ce806df37cfc250a
ryym <ryym.64@gmail.com>
2017-02-12 14:38:43 +0900

    chore(root-bin): Add root-bin to share some modules

---
+++ root-bin/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+
+/**
+ * 1. Install this package using a relative path in each package
+ *    (yarn add --dev --force file:../../root-bin)
+ * 2. Run the `root` command to use packages in the root `node_modules`
+ *    ($(npm bin)/root eslint src)
+ */
+
+const path = require('path');
+const { spawn } = require('child_process');
+
+const REPO_TO_ROOT_PATH = '../../';
+const NPM_BIN_PATH = 'node_modules/.bin';
+const toRepoPath = binPath => path.resolve(binPath, '../../../');
+const toRootRepoPath = repoPath => path.resolve(repoPath, REPO_TO_ROOT_PATH);
+
+const { argv } = process;
+const binPath = argv[1];  // A path like `/xx/packages/repo/node_modules/.bin/root`
+const command = argv[2];
+const options = argv.slice(3);
+
+const rootPath = toRootRepoPath(toRepoPath(binPath));
+const commandPath = `${rootPath}/${NPM_BIN_PATH}/${command}`;
+
+console.log(`${commandPath} ${options.join(' ')}`);
+spawn(commandPath, options, {
+  stdio: 'inherit',
+});
